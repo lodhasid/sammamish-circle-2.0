0ee8791682c772426ed32c8d987cbceba297b960
Siddhant Lodha <siddhant.lodha6@gmail.com>
2026-02-28 19:43:48 -0800

    Revert "fix"
    This reverts commit bd4b21203fd468daad6c07104a90d51eae0790fa.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic";
 import React, { useEffect, useState } from 'react';
 import { supabase } from 'utils/supabase';
 import { useRouter } from 'next/navigation';
